ebcde502f7a19469d90847f0aed9e523950f205a
Alon Kafri <alon@illusivenetworks.com>
2020-07-20 12:22:02 +0300

    CR fixes

--- src/main/java/illusive/networks/dto/BaseDTO.java
+++ src/main/java/illusive/networks/dto/BaseDTO.java
@@ -1,41 +1,19 @@
 package illusive.networks.dto;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.UUID;
 
 /**
  * Created by alon on 1/3/2020.
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public abstract class BaseDTO {
 
     protected UUID id;
 
-    public BaseDTO(UUID id) {
-        this.id = id;
-    }
-
-    public BaseDTO() {
-
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BaseDTO baseDTO = (BaseDTO) o;
-        return Objects.equals(id, baseDTO.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
--- src/main/java/illusive/networks/dto/setting/SettingDTO.java
+++ src/main/java/illusive/networks/dto/setting/SettingDTO.java
@@ -1,45 +1,18 @@
 package illusive.networks.dto.setting;
 
 import illusive.networks.dto.BaseDTO;
-
-import java.util.Objects;
-import java.util.UUID;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by alon on 1/2/2020.
  */
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class SettingDTO extends BaseDTO {
 
     private String member;
 
-    public SettingDTO(UUID id, String member) {
-        super(id);
-        this.member = member;
-    }
-
-    public SettingDTO() {
-        super();
-    }
-
-    public String getMember() {
-        return member;
-    }
-
-    public void setMember(String member) {
-        this.member = member;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        SettingDTO that = (SettingDTO) o;
-        return Objects.equals(member, that.member);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), member);
-    }
 }
